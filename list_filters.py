adfae7dc2e34f5c778b5197db3e5fe2270555f9e
Will S <wsha.code@gmail.com>
2016-06-08 03:33:15 -0400

    Add license text

--- list_filters.py
+++ list_filters.py
@@ -1,5 +1,21 @@
 """Set of filters for selecting a subset of a list"""
 
+#  This file is part of tarsnap_update
+#
+# tarsnap_update is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# tarsnap_update is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2016, willsALMANJ
 
 def sort_by_bounds(target, bounds):
     """Sort target into a list of sublists with the distance of all elements in
